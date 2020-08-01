473a3e48ae54ff9d9f66bbc0a81c1e39476370ea
balesniy <balesniy@yandex.ru>
2020-08-01 10:15:01 +0300

    добавляет composer

--- src/task_test.php
+++ src/task_test.php
@@ -1,11 +1,11 @@
 <?php
+require('../vendor/autoload.php');
+use TaskForce\Model\Task;
 
-require('Task.php');
-
-use TaskForce\Task;
-
-$task = new Task(1,1);
-assert($task->buyer_id == 1444, 'хранит buyer id');
-assert(Task::action_map[Task::STATUS_CANCEL] == null, 'содержит status cancel');
+$task = new Task(Task::STATUS_NEW,1);
+assert($task->customer_id == 1, 'хранит id заказчика');
+assert($task->get_action(1) == Task::ACTION_CANCEL, 'возвращает действие');
+assert(Task::ACTION_NAME[$task->get_action(1)] == 'Отменить', 'называет действие');
+assert(Task::get_next_status($task->get_action(1)) == Task::STATUS_CANCEL, 'возвращает следующий статус');
 
 echo 'done';
