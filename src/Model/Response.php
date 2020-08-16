3abef0b1cb6c6dbe8d2553aa3166d6a8dfb83861
balesniy <balesniy@yandex.ru>
2020-08-16 20:00:06 +0300

    добавляет проект методов классов

---
+++ src/Model/Response.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace TaskForce\Model;
+
+
+class Response
+{
+    public function changeStatus($status)
+    {
+        // устанавливает статус для отклика
+    }
+}
---
+++ src/Model/User.php
@@ -0,0 +1,38 @@
+<?php
+
+
+namespace TaskForce\Model;
+
+
+class User
+{
+
+//    проект методов класса
+
+    public function changeUser ($user)
+    {
+        // обновляет данные пользователя
+    }
+
+    public function getProfile ()
+    {
+        // возвращает данные профиля пользователя
+    }
+
+    public function getResponses ()
+    {
+        // возвращает список отывов для пользователя
+    }
+
+    public function setLastActivity ()
+    {
+        // устанавливает время последней активности на сайте
+    }
+
+    public function getRating ()
+    {
+        // рассчитывает рейтинг пользователя
+    }
+
+
+}
