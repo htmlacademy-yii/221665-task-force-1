76943bf07d613739f9732bb894539a1f75984cd7
balesniy <balesniy@yandex.ru>
2020-09-11 16:06:00 +0300

    фикстуры с городами

--- common/fixtures/CitiesFixture.php
+++ common/fixtures/CitiesFixture.php
@@ -3,7 +3,7 @@ namespace common\fixtures;
 
 use yii\test\ActiveFixture;
 
-class UserFixture extends ActiveFixture
+class CitiesFixture extends ActiveFixture
 {
-    public $modelClass = 'frontend\models\Users';
+    public $modelClass = 'frontend\models\Cities';
 }
