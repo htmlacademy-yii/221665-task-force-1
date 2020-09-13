23c51c8adca41e51266762e9421a84bda784f22c
balesniy <balesniy@yandex.ru>
2020-09-14 01:20:22 +0300

    пользователи с категорями

--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -3,13 +3,15 @@
 namespace frontend\controllers;
 
 use frontend\models\Users;
+use frontend\models\UsersCategories;
 
 class UsersController extends \yii\web\Controller
 {
     public function actionIndex()
     {
-        $users = Users::find()->all();
+        // TODO упростить запрос
+        $usersCategoriesQuery = UsersCategories::find()->select('user_id')->distinct();
+        $users = Users::find()->where(['id' => $usersCategoriesQuery])->all();
         return $this->render('index', ['users' => $users]);
     }
-
 }
